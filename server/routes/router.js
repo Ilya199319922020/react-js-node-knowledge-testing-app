48270278cac0e05aa3be537987be86f2725593bc
Ilya Smirnov <iliasmirnov93@mail.ru>
2022-12-06 02:07:02 +0300

    fixed bags

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import Answers from '../models/Answers.js';
 import Users from '../models/Users.js';
 
@@ -8,10 +7,9 @@ const router = express.Router();
 router.post('/set', async (req, res) => {
 	try {
 		const arrValue = req.body.answers;
-		
 		const [model] = await Answers.find();
-		const [modelUser] = await Users.find({ id: mongoose.Types.ObjectId(req.body.id) });
-		
+		const [modelUser] = await Users.find({ _id: req.body.id });
+
 		if (!modelUser) {
 			res.status(403).json('Не заполнены поля');
 		}
@@ -25,7 +23,7 @@ router.post('/set', async (req, res) => {
 		modelUser.answers.push(...result);
 		await modelUser.save();
 
-		res.status(200).json(modelUser)
+		res.status(201).json(modelUser)
 	} catch (error) {
 		res.status(403).json(console.log(error), 'Ошибка на клиенте')
 	}
@@ -44,6 +42,42 @@ router.post('/createUser', async (req, res) => {
 		res.status(201).json(modelUser)
 	} catch (error) {
 		res.status(403).json(console.log(error), 'Ошибка при создании пользователя')
+	}
+});
+
+router.get('/user', async (req, res) => {
+	try {
+		const modelUser = await Users.find();
+		if (!modelUser.length) {
+			res.status(403).json('Нет зарегестрированных пользователей');
+		}
+
+		const arrAnswers = modelUser.map(a => a.answers);
+		const arr = arrAnswers.filter(h => !!h.length);
+
+		const resResults = [
+			{ id: 1, right: [], wrong: [] },
+			{ id: 2, right: [], wrong: [] },
+			{ id: 3, right: [], wrong: [] },
+			{ id: 4, right: [], wrong: [] },
+			{ id: 5, right: [], wrong: [] },
+		]
+		for (let d of arr) {
+			for (let i = 0; i < d.length; i++) {
+				for (let a = 0; a < resResults.length; a++) {
+					if (a === i && d[i] === true) {
+						resResults[a].right.push(d[i]);
+					} else if (a === i && d[i] === false) {
+						resResults[a].wrong.push(d[i]);
+					}
+				}
+			}
+		}
+
+		const objData = { resResults, count: arrAnswers.length }
+		res.status(200).json(objData);
+	} catch (error) {
+		res.status(403).json(console.log(error), 'Ошибка на клиенте')
 	}
 });
 
